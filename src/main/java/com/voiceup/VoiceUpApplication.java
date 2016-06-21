ae9587aa57e45943d8019e3d450df7680fafa8af
Knut Angenendt <knut@debian>
2016-06-21 11:38:56 +0200

    init voiceup project

---
+++ src/main/java/com/voiceup/VoiceUpApplication.java
@@ -0,0 +1,13 @@
+package com.voiceup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceUpApplication {
+
+public static void main(String[] args) {
+		
+		SpringApplication.run(VoiceUpApplication.class, args);
+	}
+}
